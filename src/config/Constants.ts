212e48e37950586b07a08d3fd2befa582f859e4a
OxytToledo <gabriel.jtoledouv@gmail.com>
2022-03-03 19:13:49 -0300

    Feat: URL-Shortener

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://lucksam:W@llid0147+@url-shortner-test.4aclp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
