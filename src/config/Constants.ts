08f62538d8174fd7d17e689c4bf2b657c51de1fb
OxytToledo <gabriel.jtoledouv@gmail.com>
2022-03-17 14:54:23 -0300

    correcting exposure error

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,6 @@
+require('dotenv').config()
+
 export const config = {
 	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:
-		'mongodb+srv://lucksam:W@llid0147+@url-shortner-test.4aclp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	MONGO_CONNECTION: `${process.env.MONGO_CONNECTION}`,
 }
